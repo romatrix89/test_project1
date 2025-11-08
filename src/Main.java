301cbd3a3a6cdf5840bbad149743dcdb8c778472
RomanTsagolov <rtsagolov@gmail.com>
2025-11-08 11:14:00 +0300

    DZ2 Работа с переменными

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Привет мир!");
         System.out.println("你好世界!");
+        String a = "Hello";
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
